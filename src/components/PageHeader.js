804783fcc8981402920879ba819a95581f31c323
Rafael Desuyo Jr <rbdesuyojr@gmail.com>
2024-08-07 06:14:41 +0800

    added component page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,14 +2,20 @@ export default function PageHeader() {
     return (
         <div className="header-group">
             <div className="header-group_content">
-                <img className="header-group_content-image"/>
+                <img className="header-group_content-image" src="./../zapier-logo.png"/>
                 <div className="header-group_content-details">
                     <p>Organization Name</p>
                     <p>43 licenses balance</p>
                 </div>
+                <div className="badge-primary badge-primary--warning">
+                    <div className="badge-primary_circle"></div>
+                    <p className="badge-primary_text">Requesting</p>
+                </div>
             </div>
             <div className="header-group_actions">
-
+                <button className="button--tertiary">Add Department</button>
+                <button className="button--secondary">Add License</button>
+                <button className="button--primary">Add License Request</button>
             </div>
         </div>
     )
